1677f94b7052ab8177f678475f36bf55286e70bd
Jonatan Jair <jonatanml24@gmail.com>
2024-06-07 12:21:35 -0500

    SE AGREGO UN ITEM AL PROTAFOLIO

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,6 +7,7 @@ export default function Portfolio() {
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', justifyContent: 'center' }}>
         <Information tittle='maestro_abad' description='Pagina web' url='http://elgranmaestroabad.com/'/>
         <Information tittle='maestro_amarres' description='Pagina web' url='http://maestroamarres.com/'/>
+        <Information tittle='prestamos' description='Aplicación movil' url='https://drive.google.com/file/d/1U4Enlgi1XY5x4SK6hizGFCYdnSYkk4oZ/view?usp=sharing'/>
       </div>
     </div>
   )
