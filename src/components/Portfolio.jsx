43be593d7c524f27b9f0dab69b0819d41cb217cb
Jonatan Jair <jonatanml24@gmail.com>
2024-06-13 20:17:19 -0500

    SE AGREGO UN NUEVO PROYECTO AL PORFOLIo

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,9 +5,10 @@ export default function Portfolio() {
     <div id='portafolio' className='portfolio'>
       <h2>MI PORTAFOLIO</h2>
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', justifyContent: 'center' }}>
-        <Information tittle='maestro_abad' description='Pagina web' url='http://elgranmaestroabad.com/'/>
-        <Information tittle='maestro_amarres' description='Pagina web' url='http://maestroamarres.com/'/>
-        <Information tittle='prestamos' description='Aplicación movil' url='https://drive.google.com/file/d/1U4Enlgi1XY5x4SK6hizGFCYdnSYkk4oZ/view?usp=sharing'/>
+        <Information tittle='prestamos' description='Aplicación movil' url='https://drive.google.com/file/d/1U4Enlgi1XY5x4SK6hizGFCYdnSYkk4oZ/view?usp=sharing' />
+        <Information tittle='superheroes' description='Pagina web' url='https://jonatandev-superheroes.netlify.app/' />
+        <Information tittle='maestro_abad' description='Pagina web' url='http://elgranmaestroabad.com/' />
+        <Information tittle='maestro_amarres' description='Pagina web' url='http://maestroamarres.com/' />
       </div>
     </div>
   )
