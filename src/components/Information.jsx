7369adccbab32aaea5f216bed605d480a6c19e30
Jonatan Jair <jonatanml24@gmail.com>
2024-07-09 22:03:40 -0500

    SE ACTUALIZO EL ENLACE DE LA APK CON LA VERSION MAS RECIENTE

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -5,10 +5,10 @@ export default function Information({ tittle,description,url='javascript:void(0)
     return (  
         <div className='information'>
             <a target={url==='javascript:void(0);'?'':'_blank'} href={url}><img src={image} alt="" ></img></a>
-            <div className='information-description'>
+            {/* <div className='information-description'>
                 <h5>{tittle.toUpperCase().replace(/_/g, " ")}</h5>
                 <label>{description}</label>
-            </div>
+            </div> */}
         </div>
     )
 }
